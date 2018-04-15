d2963a02b211635d121d997d3c37c11c4c8f44c7
= <ajf93@pitt.edu>
2018-04-15 14:05:35 -0400

    Added multiplayer (hotseat) functionality.

--- JustGolf/Assets/_Scripts/BallController.cs
+++ JustGolf/Assets/_Scripts/BallController.cs
@@ -23,6 +23,25 @@ public class BallController : MonoBehaviour {
     public float exp = -0.00879889f;
     public float offset = 10;
 
+    private bool isTurn = false;
+
+    private enum BallState
+    {
+        WAITTOSTART,
+        MOVING,
+        STOPPED
+    }
+
+    private BallState ballState = BallState.WAITTOSTART;
+
+    public void StartTurn()
+    {
+        isTurn = true;
+        ballState = BallState.WAITTOSTART;
+
+        cameraman.gameObject.SetActive(true);
+    }
+
     // Use this for initialization
     void Start () {
 		rb = gameObject.GetComponent<Rigidbody> ();
@@ -48,7 +67,7 @@ public class BallController : MonoBehaviour {
         string t = m[1];
         long dur;
         bool valid = long.TryParse(t, out dur);
-        if (valid)
+        if (valid && ballState == BallState.WAITTOSTART)
         {
             float speed = MaxSpeed * CalcPower(dur / 1000);
             Debug.Log("Triggered: " + dur);
@@ -58,6 +77,9 @@ public class BallController : MonoBehaviour {
                 rb.AddForce(speed * cameraman.forward);
             }
 
+            ballState = BallState.MOVING;
+            cameraman.gameObject.SetActive(false);
+
             controller.TryReset();
         }
     }
@@ -65,48 +87,70 @@ public class BallController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        useController = controller.IsConnected();
-
-        if (useController)
+        if (isTurn)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            useController = controller.IsConnected();
+
+            if (useController)
             {
-                controller.TryReset();
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    controller.TryReset();
+                }
+            }
+
+            if(ballState == BallState.MOVING)
+            {
+                if(rb.velocity == Vector3.zero)
+                {
+                    ballState = BallState.STOPPED;
+                    cameraman.gameObject.SetActive(false);
+                    GameManager.instance.EndTurn();
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.W) && ballState == BallState.WAITTOSTART)
+            {
+                tm = 0;
+                held = true;
+                indicators[0].GetComponent<MeshRenderer>().enabled = true;
+            }
+
+            if (held && tm < MaxSpeed)
+            {
+                tm += 0.1f;
+            }
+
+            for (int i = 0; i < indicators.Length; i++)
+            {
+                if (tm > MaxSpeed / (MaxSpeed / (i + 1)))
+                    indicators[i].GetComponent<MeshRenderer>().enabled = true;
+            }
+
+            if (Input.GetKeyUp(KeyCode.W) && ballState == BallState.WAITTOSTART && held)
+            {
+                rb.AddForce(500 * tm * cameraman.forward);
+                tm = 0;
+                held = false;
+                for (int i = 0; i < indicators.Length; i++)
+                {
+                    indicators[i].GetComponent<MeshRenderer>().enabled = false;
+                }
+
+                ballState = BallState.MOVING;
+            }
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                rb.velocity = Vector3.zero;
+                tm = 0;
+                held = false;
+                for (int i = 0; i < indicators.Length; i++)
+                {
+                    indicators[i].GetComponent<MeshRenderer>().enabled = false;
+                }
             }
         }
-
-		if (Input.GetKeyDown(KeyCode.W) && rb.velocity == Vector3.zero) {
-			tm = 0;
-			held = true;
-			indicators [0].GetComponent<MeshRenderer> ().enabled = true;
-		}
-
-		if (held && tm < MaxSpeed) {
-			tm += 0.1f;
-		}
-
-		for (int i = 0; i < indicators.Length; i++) {
-			if(tm > MaxSpeed/(MaxSpeed/(i+1)))
-				indicators [i].GetComponent<MeshRenderer> ().enabled = true;
-		}
-
-		if (Input.GetKeyUp(KeyCode.W)) {
-			rb.AddForce (500*tm*cameraman.forward);
-			tm = 0;
-			held = false;
-			for (int i = 0; i < indicators.Length; i++) {
-				indicators [i].GetComponent<MeshRenderer> ().enabled = false;
-			}
-		}
-
-		if (Input.GetKeyDown (KeyCode.F)) {
-			rb.velocity = Vector3.zero;
-			tm = 0;
-			held = false;
-			for (int i = 0; i < indicators.Length; i++) {
-				indicators [i].GetComponent<MeshRenderer> ().enabled = false;
-			}
-		}
 	}
 
 }
---
+++ JustGolf/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
+    private int level = 0;                                  //Current level number, expressed in game as "Day 1".
+
+    public Camera camera;
+
+    private int currentPlayer = 0;
+    public List<BallController> Players;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        camera.gameObject.SetActive(false);
+        Players[currentPlayer].StartTurn();
+
+    }
+
+    public void EndTurn()
+    {
+        currentPlayer = (currentPlayer + 1) % Players.Count;
+
+        Players[currentPlayer].StartTurn();
+    }
+
+}
