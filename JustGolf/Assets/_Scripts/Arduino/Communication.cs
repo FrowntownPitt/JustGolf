4055393c8bb2c79e38643f78d5f6a06e9d60d82c
= <ajf93@pitt.edu>
2018-04-15 00:34:21 -0400

    Added controller functionality to scene

---
+++ JustGolf/Assets/_Scripts/Arduino/Communication.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Arduino
+{
+    public class Communication : MonoBehaviour
+    {
+
+        public string port = "COM5";
+        USB ArduinoHandler;
+
+        float prevTime;
+
+        public float pulse;
+
+        Coroutine listenerCoroutine;
+        Coroutine resetCoroutine;
+
+        public Dictionary<string, System.Action<string>> messageHandlers;
+
+        public bool IsConnected()
+        {
+            return ArduinoHandler.IsOpen();
+        }
+
+        private void Start()
+        {
+            messageHandlers = new Dictionary<string, System.Action<string>>();
+        }
+
+        public void AddHandler(string token, System.Action<string> handler)
+        {
+            messageHandlers.Add(token, handler);
+        }
+
+        // Use this for initialization
+        void OnEnable()
+        {
+            ArduinoHandler = new USB(port);
+
+            ArduinoHandler.Open();
+
+            //StartCoroutine(TriggerCheck());
+            StartListener();
+        }
+
+        IEnumerator TriggerCheck()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(2);
+
+                StopListener();
+                StartListener(2000f);
+            }
+        }
+
+        /*void TriggerHandler(string message)
+        {
+            //if(resetCoroutine == null)
+            //    resetCoroutine = StartCoroutine(HandleTrigger());
+        }*/
+
+        void MessageHandler(string message)
+        {
+            Debug.Log("Message: " + message);
+            bool foundHandler = false;
+            foreach(string h in messageHandlers.Keys)
+            {
+                if (message.Contains(h))
+                {
+                    messageHandlers[h](message);
+                    foundHandler = true;
+                }
+            }
+            /*if (message.Contains("TRIGGER"))
+            {
+                TriggerHandler(message);
+            }*/
+            if(!foundHandler)
+            {
+                StopListener();
+                StartListener();
+            }
+        }
+
+        public void TryReset()
+        {
+            ResetTrigger();
+        }
+
+        /*IEnumerator HandleTrigger()
+        {
+            StopListener();
+
+
+            bool doReset = true;
+
+            WaitForSeconds waitForSeconds = new WaitForSeconds(.1f);
+
+            WriteToArduino("CHECKRESET", 1);
+
+            string response = ArduinoHandler.ReadFromArduino(5000);
+
+            Debug.Log("CHECKRESET: " + response);
+
+            string[] m = response.Split();
+
+
+            if (m.Length != 2)
+            {
+                Debug.Log("CHECKRESET Response malformed!");
+            }
+            else
+            {
+                if (m[1].Equals("TRUE"))
+                {
+                    doReset = false;
+                    Debug.Log("Already Reset!");
+                }
+                if (m[1].Equals("FALSE"))
+                {
+                    doReset = true;
+                    ResetTrigger();
+                }
+            }
+
+            while (doReset)
+            {
+                WriteToArduino("CHECKRESET", 1);
+
+                response = ArduinoHandler.ReadFromArduino(5000);
+
+                Debug.Log("CHECKRESET: " + response);
+
+                m = response.Split();
+
+                if(m.Length != 2)
+                {
+                    Debug.Log("CHECKRESET Response malformed!");
+                }
+                else
+                {
+                    if (m[1].Equals("TRUE"))
+                    {
+                        doReset = false;
+                        Debug.Log("Finished reset!");
+                        break;
+                    }
+                    if (m[1].Equals("FALSE"))
+                    {
+                    }
+                }
+
+                yield return waitForSeconds;
+            }
+
+            Debug.Log("Reset finished!");
+
+            StartListener();
+
+            resetCoroutine = null;
+
+            yield return null;
+        }*/
+
+        void ReadErrorHandler()
+        {
+            Debug.Log("Error!");
+            StopListener();
+            ResetTrigger();
+            StartListener();
+        }
+
+        void ResetTrigger()
+        {
+            WriteToArduino("RESETTRIGGER", 1);
+            //WriteToArduino("RESETTRIGGER");
+        }
+
+        void StopListener()
+        {
+            if (listenerCoroutine != null)
+            {
+                StopCoroutine(listenerCoroutine);
+            }
+        }
+
+        void StartListener(float time = float.PositiveInfinity)
+        {
+            Debug.Log("Starting Arduino Listener");
+            listenerCoroutine = StartCoroutine(ArduinoHandler.AsynchronousReadFromArduino(
+                (string s) => MessageHandler(s), () => ReadErrorHandler(), time));
+        }
+
+        public void WriteToArduino(string message, int priority = 0)
+        {
+            if (Time.time - prevTime > pulse || priority > 0)
+            {
+                ArduinoHandler.WriteToArduino(message);
+                prevTime = Time.time + pulse;
+            }
+        }
+
+        void OnDisable()
+        {
+            WriteToArduino("RESETSTEP", 1);
+            WriteToArduino("RESETTRIGGER", 1);
+            ArduinoHandler.Close();
+        }
+    }
+}
--- JustGolf/Assets/_Scripts/BallController.cs
+++ JustGolf/Assets/_Scripts/BallController.cs
@@ -1,8 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Arduino;
 
 public class BallController : MonoBehaviour {
+
+    [SerializeField]
+    Communication controller;
+    bool useController;
+
 	Rigidbody rb;
 	Transform tf;
 
@@ -13,8 +19,12 @@ public class BallController : MonoBehaviour {
 	public GameObject[] indicators;
 	public float MaxSpeed;
 
-	// Use this for initialization
-	void Start () {
+    public float mult = 118.768f;
+    public float exp = -0.00879889f;
+    public float offset = 10;
+
+    // Use this for initialization
+    void Start () {
 		rb = gameObject.GetComponent<Rigidbody> ();
 		tf = gameObject.transform;
 		held = false;
@@ -22,10 +32,47 @@ public class BallController : MonoBehaviour {
 		for (int i = 0; i < indicators.Length; i++) {
 			indicators [i].GetComponent<MeshRenderer> ().enabled = false;
 		}
-	}
+
+        controller.AddHandler("TRIGGER", (string s) => HandleBallHit(s));
+    }
+
+    public float CalcPower(long time)
+    {
+        return mult * (Mathf.Exp(exp * time)) + offset;
+    }
+
+
+    private void HandleBallHit(string message)
+    {
+        string[] m = message.Split();
+        string t = m[1];
+        long dur;
+        bool valid = long.TryParse(t, out dur);
+        if (valid)
+        {
+            float speed = MaxSpeed * CalcPower(dur / 1000);
+            Debug.Log("Triggered: " + dur);
+            Debug.Log("Power: " + speed);
+
+            if (useController) {
+                rb.AddForce(speed * cameraman.forward);
+            }
+        }
+    }
 
 	// Update is called once per frame
 	void Update () {
+
+        useController = controller.IsConnected();
+
+        if (useController)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                controller.TryReset();
+            }
+        }
+
 		if (Input.GetKeyDown(KeyCode.W) && rb.velocity == Vector3.zero) {
 			tm = 0;
 			held = true;
