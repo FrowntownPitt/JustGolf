918719a9e5a3ec2cd25f027d26e2342aa18e4517
= <ajf93@pitt.edu>
2018-04-17 21:24:51 -0400

    Updated documentation

--- JustGolf/Assets/_Scripts/BallController.cs
+++ JustGolf/Assets/_Scripts/BallController.cs
@@ -6,37 +6,39 @@ using Arduino;
 public class BallController : MonoBehaviour {
 
     [SerializeField]
-    Communication controller;
-    bool useController;
+    Communication controller; // Use the global arduino controller script
+    bool useController; // Keep track of whether the game is using the USB controller
 
 	Rigidbody rb;
 	Transform tf;
 
-	public Transform cameraman;
-	float tm;
-	bool held;
+	public Transform cameraman;  // Camera which follows the player
+	float tm;   // How long the user has held the fire key (keyboard controls)
+	bool held;  // Track when the user holds the fire key (keyboard controls)
 
-    public int score = 0;
-    public int cumulativeScore = 0;
+    public int score = 0;               // player's level score
+    public int cumulativeScore = 0;     // Track player's persistent score
 
-	public GameObject[] indicators;
-	public float MaxSpeed;
+	public GameObject[] indicators;     // Show how long the user has pressed the fire key
+	public float MaxSpeed;  // Max speed the player can travel (keyboard controls)
 
+    // Arduino controller's exponential function (time to speed) [m*e^(j*x) + k]
     public float mult = 118.768f;
     public float exp = -0.00879889f;
-    public float offset = 10;
+    public float offset = 10;   // Minimum speed offset
 
-    private bool isTurn = false;
+    private bool isTurn = false; // Whether it is this player's turn
 
-    public bool isFinished = false;
+    public bool isFinished = false; // Whether this player has reached the hole
 
-    private enum BallState
+    private enum BallState // State machine of the turn
     {
-        WAITTOSTART,
-        MOVING,
-        STOPPED
+        WAITTOSTART, // Waiting for input
+        MOVING,      // In the process of moving
+        STOPPED      // Stopped moving
     }
 
+    // Reset at the beginning of the game
     public void ResetPlayer()
     {
         score = 0;
@@ -48,30 +50,32 @@ public class BallController : MonoBehaviour {
         ballState = BallState.WAITTOSTART;
     }
 
-    private BallState ballState = BallState.WAITTOSTART;
+    private BallState ballState = BallState.WAITTOSTART; // State machine
 
 	public float hitStart = 0;
     public float currentTime;// = Time.time;
 
+    // Enable this player's turn
     public void StartTurn()
     {
         isTurn = true;
-        ballState = BallState.WAITTOSTART;
+        ballState = BallState.WAITTOSTART; // Init state machine
 
-        controller.TryReset();
+        controller.TryReset(); // Reset the controller (replace the ball)
 
-        cameraman.gameObject.SetActive(true);
+        cameraman.gameObject.SetActive(true); // Enable this player's camera
     }
 
+    // Stop this player's turn
     public void EndTurn()
     {
         isTurn = false;
         ballState = BallState.WAITTOSTART;
 
+        // disable this player's camera
         cameraman.gameObject.SetActive(false);
     }
-
-    // Use this for initialization
+    
     void Start () {
 		rb = gameObject.GetComponent<Rigidbody> ();
 		tf = gameObject.transform;
@@ -88,36 +92,43 @@ public class BallController : MonoBehaviour {
         //controller.AddHandler("TRIGGER", (string s) => HandleBallHit(s));
     }
 
+    // Do the exponential time-to-power function
+    // time: "<time in ms>"
     public float CalcPower(long time)
     {
         return mult * (Mathf.Exp(exp * time)) + offset;
     }
     
+    // What to do when the controller says the ball has been hit
+    // message: "TRIGGER <time in us>"
     public void HandleBallHit(string message)
     {
+        // Get the time from the message
         string[] m = message.Split();
         string t = m[1];
         long dur;
         bool valid = long.TryParse(t, out dur);
+
+        // If the message is valid and the ball hasn't started moving
         if (valid && ballState == BallState.WAITTOSTART)
         {
+            // Calculate the speed the user hit the ball
             float speed = MaxSpeed * CalcPower(dur / 1000);
             Debug.Log("Triggered: " + dur);
             Debug.Log("Power: " + speed);
 
             if (useController) {
+                // Move at the speed the user hit, in the direction they hit
                 rb.AddForce(speed * cameraman.forward);
             }
 
-            ballState = BallState.MOVING;
-			hitStart = Time.time;
-            //cameraman.gameObject.SetActive(false);
 
-            //controller.TryReset();
+            ballState = BallState.MOVING; // Say the ball is moving now
+
+			hitStart = Time.time; // Track the time the user has hit the ball
         }
     }
-
-	// Update is called once per frame
+    
 	void Update () {
 		currentTime = Time.time;
         if (isTurn)
@@ -126,6 +137,7 @@ public class BallController : MonoBehaviour {
 
             if (useController)
             {
+                // In case the controller is not reset, allow the user to press space to reset it
                 if (Input.GetKeyDown(KeyCode.Space))
                 {
                     controller.TryReset();
@@ -134,21 +146,26 @@ public class BallController : MonoBehaviour {
 
             if(ballState == BallState.MOVING)
             {
+                // once the ball has stopped moving, track that
                 if(rb.velocity == Vector3.zero)
                 {
-					//Debug.Log ("Checking");
-					//Debug.Log ("Start Time: " + hitStart);
-					//Debug.Log("Current Time: " + currentTime);
-					if (currentTime - hitStart > 3) {
-						//Debug.Log ("Got it");
-						hitStart = float.MaxValue;
+                    // Only allow after the ball has been moving for 3 seconds
+                    // (traps a bug)
+					if (currentTime - hitStart > 3)
+                    {
+                        // Track the number of attempts the user has made
+                        score++;
+                        cumulativeScore++;
+
+                        hitStart = float.MaxValue;
 						ballState = BallState.STOPPED;
-						cameraman.gameObject.SetActive (false);
-						GameManager.instance.EndTurn ();
+						cameraman.gameObject.SetActive (false); // Disable the player's camera
+						GameManager.instance.EndTurn (); // End the current player's turn
 					}
                 }
             }
 
+            // If the user wants to use the keyboard, track the "w" key pressed for power
             if (Input.GetKeyDown(KeyCode.W) && ballState == BallState.WAITTOSTART)
             {
                 tm = 0;
@@ -158,21 +175,24 @@ public class BallController : MonoBehaviour {
 
             if (held && tm < MaxSpeed)
             {
-                tm += 0.1f;
+                tm += 0.1f; // track the time the user holds 'w'
             }
 
+            // Indicate the current power the player has set
             for (int i = 0; i < indicators.Length; i++)
             {
                 if (tm > MaxSpeed / (MaxSpeed / (i + 1)))
                     indicators[i].GetComponent<MeshRenderer>().enabled = true;
             }
 
+            // When the player releases the 'w' key, hit the ball
             if (Input.GetKeyUp(KeyCode.W) && ballState == BallState.WAITTOSTART && held)
             {
 				hitStart = currentTime;
                 rb.AddForce(500 * tm * cameraman.forward);
                 tm = 0;
                 held = false;
+                // disable the power indicators
                 for (int i = 0; i < indicators.Length; i++)
                 {
                     indicators[i].GetComponent<MeshRenderer>().enabled = false;
@@ -181,6 +201,7 @@ public class BallController : MonoBehaviour {
                 ballState = BallState.MOVING;
             }
 
+            // On 'f', stop the velocity (for debugging/testing)
             if (Input.GetKeyDown(KeyCode.F))
             {
                 rb.velocity = Vector3.zero;
@@ -194,7 +215,7 @@ public class BallController : MonoBehaviour {
         }
 	}
 
-
+    // When the ball reaches the end, or interacts with the level components
     private void OnTriggerEnter(Collider other)
     {
 		if (other.CompareTag ("Finish")) {
--- JustGolf/Assets/_Scripts/Managers/GameManager.cs
+++ JustGolf/Assets/_Scripts/Managers/GameManager.cs
@@ -9,44 +9,51 @@ public class GameManager : MonoBehaviour {
     public static GameManager instance = null;              //Static instance of GameManager which allows it to be accessed by any other script.
     private int level = 1;                                  //Current level number, expressed in game as "Day 1".
 
-    public Camera camera;
+    public Camera camera;       // Camera of the scene, disable on switch to a player's camera
 
-    private int currentActivePlayer = 0;
-    private int currentPlayer = 0;
-    public List<BallController> Players;
-    public List<BallController> RemainingPlayers;
+    private int currentActivePlayer = 0;            // Round robin, track the current global player
+    private int currentPlayer = 0;                  // Round robin, track the current remaining player
+    public List<BallController> Players;            // Keep track of all of the players
+    public List<BallController> RemainingPlayers;   // Track players which have not reached the end 
 
+    [SerializeField]
+    private bool returnToMain; // Was this level played standalone, or part of a series?
 
-    public List<string> Levels;
+    public List<string> Levels; // Define what the level names are, in order
     
     [SerializeField]
-    public static Arduino.Communication gameController;
-    public string controllerPort = "COM5";
+    public static Arduino.Communication gameController;     // Arduino communication handler
+    public string controllerPort = "COM5";                  // Which COM should we ask for?
 
-    public GameObject playerNotification;
-    public Text playerText;
+    public GameObject playerNotification;   // A "toast" to whichever player's turn it is
+    public Text playerText;     // Text to display on the toast
 
     void Awake()
     {
+        // Begin singleton pattern
         if (instance == null)
             instance = this;
         else if (instance != this)
             Destroy(gameObject);
         
         DontDestroyOnLoad(gameObject);
+        // end singleton pattern
+
+        // Make the players persistent
         for (int i = 0; i < Players.Count; i++)
         {
             DontDestroyOnLoad(Players[i].gameObject.transform.root);
             Players[i].score = 0;
         }
 
+        // Initialize our handler
         gameController = GetComponent<Arduino.Communication>();
 
+        // Do level initialization
         InitLevel();
-        //gameController = new Arduino.Communication();
-        //gameController.TryConnect(controllerPort);
     }
 
+    // UNUSED: Disable players from moving
     public void DisablePlaying()
     {
         for(int i=0; i<Players.Count; i++)
@@ -55,41 +62,51 @@ public class GameManager : MonoBehaviour {
         }
     }
 
+    // UNUSED: Allow the players to move
     public void EnablePlaying()
     {
 
     }
 
+    // Prime the system for the first level
     public void StartGame()
     {
         level = 1;
         //StartLevel(level);
     }
 
+    // UNUSED: Reset the game
     public void ResetGame()
     {
+        // Reset player data
         for(int i=0; i<Players.Count; i++)
         {
             Players[i].ResetPlayer();
         }
     }
 
+    // Level initialization
     public void InitLevel()
     {
+        // Copy Players to RemainingPlayers
         RemainingPlayers = new List<BallController>(Players.Count);
         for (int i = 0; i < Players.Count; i++)
         {
             RemainingPlayers.Insert(i, Players[i]);
-            Players[i].isFinished = false;
-            Players[i].score = 0;
+            Players[i].isFinished = false;  // Each player hasn't finished the new level yet
+            Players[i].score = 0;           // Each player's level score is reset
             //DontDestroyOnLoad(Players[i].gameObject.transform.root);
         }
+
+        // Start on the first player
         currentActivePlayer = 0;
         currentPlayer = 0;
 
+        // Let the first player start
         StartNextTurn();
     }
 
+    // Go through each player and place them on that player's location in the next level
     public void PlacePlayers(List<GameObject> locations)
     {
         for(int i=0; i<Players.Count; i++)
@@ -112,6 +129,7 @@ public class GameManager : MonoBehaviour {
             t.rotation = s.rotation;
             t.localScale = s.localScale;
 
+            // Destroy the placeholder player if there is already a persistent player
             if (s.root != Players[i].gameObject.transform.root) Destroy(locations[i]);
             else Debug.Log("First level!" + level);
         }
@@ -120,21 +138,27 @@ public class GameManager : MonoBehaviour {
 
         //StartNextTurn();
     }
-
-    private void StartLevel(int level)
+    
+    // Start the given level. Run in "trial mode" if single=true (play that level then return to main menu)
+    public void StartLevel(int level, bool single=false)
     {
-        SceneManager.LoadSceneAsync(Levels[level]);
+        returnToMain = single;
+
+        // Just load the scene. Persistent objects will survive.
+        SceneManager.LoadSceneAsync(Levels[level]); 
         //Application.LoadLevelAdditiveAsync(Levels[level]);
     }
 
     private void Start()
     {
         camera.gameObject.SetActive(false);
-        StartNextTurn();
+        StartNextTurn(); // Start the first player
         //Players[currentPlayer].StartTurn();
 
+        // Connect to the controller
         Debug.Log(gameController.TryConnect(controllerPort));
 
+        // Add the global controller handler as a callback for the TRIGGER message
         gameController.AddHandler("TRIGGER", (string s) => HandleBallHit(s));
 
         //System.Action<string> H = (string s) => this.HandleBallHit(s);
@@ -143,26 +167,38 @@ public class GameManager : MonoBehaviour {
 
     }
 
+    // The controller handler for when the ball is hit. Tell the current player the ball has been hit
     public void HandleBallHit(string message)
     {
         RemainingPlayers[currentPlayer].HandleBallHit(message);
-
         //Debug.Log("Triggered!!!!");
     }
 
+    // Called when all players have finished the level.
     private void EndLevel()
     {
-        StartLevel(++level);
+        if (returnToMain) // If we played this level in "Trial mode", return to the main menu
+        {
+            level = 0;
+            StartLevel(level);
+        }
+        else
+        {
+            StartLevel(++level); // Otherwise, start the next level
+        }
     }
 
+    // End the current player's turn and begin the next
+    // Called by the player once it has stopped moving
     public void EndTurn()
     {
+        // When it's reached the hole, remove it from the active players
         if (RemainingPlayers[currentPlayer].isFinished)
         {
             RemainingPlayers.RemoveAt(currentPlayer);
             if (RemainingPlayers.Count == 0)
             {
-                EndLevel();
+                EndLevel(); // End the current level if all players have reached the hole
                 return;
             }
             else
@@ -171,9 +207,10 @@ public class GameManager : MonoBehaviour {
             }
         }
         else {
-            currentPlayer = (currentPlayer + 1) % RemainingPlayers.Count;
+            currentPlayer = (currentPlayer + 1) % RemainingPlayers.Count; // Go to the next remaining player
         }
 
+        // Figure out what the next player is
         int cur = currentActivePlayer;
         for (int i = (cur+1)%Players.Count; i != cur; i = (i + 1) % Players.Count)
         {
@@ -181,15 +218,19 @@ public class GameManager : MonoBehaviour {
             currentActivePlayer = (currentActivePlayer + 1) % Players.Count;
         }
 
+        // Begin the next player's turn
         StartNextTurn();
         //Players[currentPlayer].StartTurn();
     }
 
+    // Begin the turn of the next player
     private void StartNextTurn()
     {
+        // Display the "Player x" toast
         playerText.text = "Player " + (currentActivePlayer+1);
         playerNotification.GetComponent<Animator>().Play("Fade Effect", -1, 0);
 
+        // Start the turn of the current player (incremented before calling this method)
         RemainingPlayers[currentPlayer].StartTurn();
     }
 
