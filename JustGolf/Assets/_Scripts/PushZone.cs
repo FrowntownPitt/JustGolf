cab2c4a3b637728902e197446f1bf96481314b70
PatHealy <pwh5@pitt.edu>
2018-04-17 23:15:25 -0400

    Added PushZone to fix Horror level

---
+++ JustGolf/Assets/_Scripts/PushZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushZone : MonoBehaviour {
+	public Vector3 direction;
+
+	void OnTriggerStay(Collider other){
+		if (other.CompareTag ("Player")) {
+			other.GetComponent<Rigidbody> ().AddForce (direction);
+		}
+	}
+
+}
